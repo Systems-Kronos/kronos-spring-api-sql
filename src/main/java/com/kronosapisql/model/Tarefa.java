a148e3091d71273d38a807a9bc67b17f4d980b8a
CaduPerrudGerminare <carlos.sousa@germinare.org.br>
2025-08-14 00:54:12 -0300

    feat: Adicionando os métodos CRUD da Habilidade

--- src/main/java/com/kronosapisql/model/Tarefa.java
+++
@@ -1,2 +0,0 @@
-package com.kronosapisql.model;public class Tarefa {
-}
---
+++ src/main/java/com/kronosapisql/service/HabilidadeService.java
@@ -0,0 +1,39 @@
+package com.kronosapisql.service;
+
+import com.kronosapisql.model.Habilidade;
+import com.kronosapisql.repository.HabilidadeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HabilidadeService {
+
+    private HabilidadeRepository habilidadeRepository;
+
+    public HabilidadeService(HabilidadeRepository habilidadeRepository) {
+        this.habilidadeRepository = habilidadeRepository;
+    }
+
+    public Optional<Habilidade> selecionarPeloId(Long id) {
+        return this.habilidadeRepository.findById(id);
+    }
+
+    public List<Habilidade> selecionar() {
+        return this.habilidadeRepository.findAll();
+    }
+
+    public Habilidade salvar(Habilidade habilidade) {
+        return this.habilidadeRepository.save(habilidade);
+    }
+
+    public void deletar(Long id) {
+        this.habilidadeRepository.deleteById(id);
+    }
+
+    public void atualizar(Habilidade habilidade) {
+        this.habilidadeRepository.save(habilidade);
+    }
+
+}
